0155261e85d3faf026598756ad38a2f0e1944c91
Samarth Shukla <samarthshukla18@gmail.com>
2023-08-27 20:02:57 +0530

    Initial commit

---
+++ src/main/java/sampleazurespringboot/sampleazurespringboot/SampleAzureSpringBootApplication.java
@@ -0,0 +1,13 @@
+package sampleazurespringboot.sampleazurespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAzureSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAzureSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sampleazurespringboot/sampleazurespringboot/SampleAzureSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package sampleazurespringboot.sampleazurespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAzureSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
